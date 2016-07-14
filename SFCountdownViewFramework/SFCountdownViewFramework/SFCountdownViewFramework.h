c5dd06f6fb62a05c9f9967fe1011ceb61202cbca
Jiang Liyin <liyinjiang@cienet.com.cn>
2016-07-14 14:33:57 +0800

    Support carthage integrate

---
+++ SFCountdownViewFramework/SFCountdownViewFramework/SFCountdownViewFramework.h
@@ -0,0 +1,20 @@
+//
+//  SFCountdownViewFramework.h
+//  SFCountdownViewFramework
+//
+//  Created by LiyingX Jiang on 7/14/16.
+//  Copyright © 2016 LiyingX Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFCountdownViewFramework.
+FOUNDATION_EXPORT double SFCountdownViewFrameworkVersionNumber;
+
+//! Project version string for SFCountdownViewFramework.
+FOUNDATION_EXPORT const unsigned char SFCountdownViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFCountdownViewFramework/PublicHeader.h>
+
+
+#import <SFCountdownViewFramework/SFCountdownView.h>
